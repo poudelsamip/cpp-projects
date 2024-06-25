2e41d7fb0b904fedf1708d0dd9f7b967812237d7
Samip Poudel <144611884+poudelsamip@users.noreply.github.com>
2024-06-25 08:39:24 +0545

    Update todo.cpp
    added file i/o to store tasks.

--- todo.cpp
+++ todo.cpp
@@ -1,15 +1,28 @@
-
 #include<iostream>
+#include<fstream>
 using namespace std;
 
-void addtask(string task[20], int* count);
-void removetask(string task[20], int* count);
+void addtask(string task[20], int& count);
+void removetask(string task[20], int& count);
 
 int main(){
-    int choice, count=0;
+    int choice;
     string task[20];
+    string newdata;
+    // ifstream newfile("myfile.txt");
+    // while(getline(newfile, newdata)){
+    //     task[count++] = newdata;
+    // }
+    // newfile.close();
+    // string data;
     do{
-        cout<<endl<<"##----- WELCOME TO TO-DO LIST APP -----##"<<endl;
+        int count = 0;
+        ifstream file("myfile.txt");
+        while(getline(file, newdata)){
+            task[count] = newdata;
+            count++;
+        }
+        system("cls");
         if(count>0){
             cout<<endl<<"<---- Your Tasks ---->"<<endl;
             for(int i=0;i<count;i++){
@@ -19,18 +32,24 @@ int main(){
         else{
             cout<<endl<<"No tasks available.";
         }
+        // int i=0;
+        // cout<<"Your tasks: "<<endl;
+        // while(getline(file,data)){
+        //     cout<<i+1<<". "<<data<<endl;
+        //     i++;
+        // }
         cout<<endl<<"1. Add task"<<endl;
         cout<<"2. Remove task"<<endl;
+        cout<<"0. Exit program"<<endl;
         cout<<endl<<"Enter your choice: ";
         cin>>choice;
-        switch (choice)
-        {
+        file.close();
+        switch (choice){
         case 1:
-            addtask(task, &count);
+            addtask(task, count);
             break;
-        
         case 2:
-            removetask(task, &count);
+            removetask(task, count);
             break;
         case 0:
             cout<<endl<<"Program Exiting ...... ";
@@ -39,27 +58,37 @@ int main(){
     return 0;
 }
 
-void addtask(string task[20], int* count){
+void addtask(string task[20], int& count){
+    system("cls");
+    fstream file("myfile.txt",ios::app);
     string todo;
     cout<<"Enter task : ";
     cin.ignore();
     getline(cin,todo);
-    task[*count] = todo;
-    (*count)++;
+    file << todo;
+    file << endl;
+    task[count] = todo;
+    count++;
     cout<<endl<<endl;
+    file.close();
     return;
 }
 
-void removetask(string task[20], int* count){
-    if(*count>0){
+void removetask(string task[20], int& count){
+    if(count>0){
         int choice;
         cout<<"Task No: ";
         cin>>choice;
-        for(int i=(choice-1);i<*count;i++){
+        for(int i=(choice-1);i<count-1;i++){
             task[i] = task[i+1];
         }
-        (*count)--;
-        return;
+        count--;
+        ofstream file("myfile.txt");
+        file.clear();
+        for(int i=0;i<count;i++){
+            file<<task[i]<<endl;
+        }
+        return ;
     }
     else{
         cout<<"No tasks to remove."<<endl;
