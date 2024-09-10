048bb8d27af7ef196269aa412dba09b397fc36f7
Samip Poudel <144611884+poudelsamip@users.noreply.github.com>
2024-09-10 20:42:33 +0545

    Update and rename to sudokusolver.cpp

--- sudokusolver.cpp
+++ sudokusolver.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void display_soduku(int soduku[9][9]);
-int check_value(int soduku[9][9], int, int, int);
-int solve_soduku(int soduku[9][9], int, int);
+void display_sudoku(int sudoku[9][9]);
+int check_value(int sudoku[9][9], int, int, int);
+int solve_sudoku(int sudoku[9][9], int, int);
 
 int main(){
 
-    int soduku[9][9] = {
+    int sudoku[9][9] = {
         {3,0,0,0,2,0,0,7,0},
         {9,0,0,5,0,0,0,1,4},
         {0,1,6,3,7,0,0,0,8},
@@ -19,23 +19,23 @@ int main(){
         {7,3,1,0,8,2,0,0,0}
     };
 
-    cout<<"\nSoduku before solving ";
-    display_soduku(soduku);
+    cout<<"\nsudoku before solving ";
+    display_sudoku(sudoku);
 
-    // cout<<check_value(soduku, 5, 0, 1);
+    // cout<<check_value(sudoku, 5, 0, 1);
 
-    if(solve_soduku(soduku, 0, 0)){
-        cout<<"The soduku is solved: \n";
-        display_soduku(soduku);
+    if(solve_sudoku(sudoku, 0, 0)){
+        cout<<"The sudoku is solved: \n";
+        display_sudoku(sudoku);
     }
     else{
-        cout<<"Soduku is not solveable";
+        cout<<"sudoku is not solveable";
     }
 
     return 0;
 }
 
-void display_soduku(int soduku[9][9]){
+void display_sudoku(int sudoku[9][9]){
 
     cout<<endl;
     cout<<" - - - - - - - - - - - - - - -\n";
@@ -43,7 +43,7 @@ void display_soduku(int soduku[9][9]){
         if(i%3 == 0 && i!=0) cout<<" - - - - - - - - - - - - - - -\n";
         for(int j=0;j<9;j++){
             if(j%3 == 0) cout<<"|";
-            cout<<" "<<soduku[i][j]<<" ";
+            cout<<" "<<sudoku[i][j]<<" ";
         }
         cout<<"|"<<endl;
     }
@@ -52,14 +52,14 @@ void display_soduku(int soduku[9][9]){
 
 }
 
-int check_value(int soduku[9][9], int value, int row, int col){
+int check_value(int sudoku[9][9], int value, int row, int col){
 
     for(int i=0;i<9;i++){
-        if(soduku[row][i] == value) return 0;
+        if(sudoku[row][i] == value) return 0;
     }
 
     for(int i=0;i<9;i++){
-        if(soduku[i][col] == value) return 0;
+        if(sudoku[i][col] == value) return 0;
     }
 
     int r = row - row % 3;
@@ -67,14 +67,14 @@ int check_value(int soduku[9][9], int value, int row, int col){
 
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            if(soduku[r+i][c+j] == value) return 0;
+            if(sudoku[r+i][c+j] == value) return 0;
         }
     }
 
     return 1;
 }
 
-int solve_soduku(int soduku[9][9], int row , int col){
+int solve_sudoku(int sudoku[9][9], int row , int col){
 
     if(col == 9){
         if(row == 8) return 1;
@@ -82,16 +82,15 @@ int solve_soduku(int soduku[9][9], int row , int col){
         col = 0;
     }
 
-
-    if(soduku[row][col] > 0) return solve_soduku(soduku, row, col+1);
+    if(sudoku[row][col] > 0) return solve_sudoku(sudoku, row, col+1);
     
     for(int i=1; i<=9; i++){
-        if(check_value(soduku, i, row, col)){
-            soduku[row][col] = i;
-            if(solve_soduku(soduku, row, col+1)){
+        if(check_value(sudoku, i, row, col)){
+            sudoku[row][col] = i;
+            if(solve_sudoku(sudoku, row, col+1)){
                return 1; 
             }
-            soduku[row][col] = 0;
+            sudoku[row][col] = 0;
         }
     }
 
